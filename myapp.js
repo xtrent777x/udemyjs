7b67bf7122129c3f85e977ba488e0f7035887555
Trent Roesbery <xtrent777x@gmail.com>
2022-01-29 09:53:57 -0700

    fuction

--- myapp.js
+++ myapp.js
@@ -1,13 +1,16 @@
-console.log( "Hello world" );
+function makeCoffee( sugar, milk )
+{
+    var instructions = "Boil water,";
+    
+    instructions += " pour into cup,";
+    
+    instructions += " add coffee granules,";
+    
+    instructions += " add " + sugar + " spoons of sugar,";
+    
+    instructions += " add " + milk + "% milk.";
+    
+    return instructions;
+}
 
-console.log( 200 );
-
-console.log( true );
-
-console.log( null );
-
-var num = 10;
-
-console.log( num );
-
-console.log( "string", 10.11, true, false, null, num );
+console.log( makeCoffee( 2, 20 ) );
