71072bceb713c4253ea1648ed1a883c5b9d46ba1
Trent Roesbery <xtrent777x@gmail.com>
2022-01-29 14:08:59 -0700

    embedding

--- myapp.js
+++ myapp.js
@@ -1,28 +1,22 @@
-// function makeCoffee( sugar, milk )
-// {
-//     var instructions = "Boil water,";
-    
-//     instructions += " pour into cup,";
-    
-//     instructions += " add coffee granules,";
-    
-//     instructions += " add " + sugar + " spoons of sugar,";
-    
-//     instructions += " add " + milk + "% milk.";
-    
-//     return instructions;
-// }
-
-// console.log( makeCoffee( 2, 20 ) );
-
 var car = {
-    color : 'red',
-    speed : 200,
-    drive : function(){return "drive";}
+    make: "volvo",
+    speed: 160,
+    engine: {
+        size: 2.0,
+        make: "bmw",
+        fuel: "petrol",
+        pistons:[
+            { maker: "BMW" },
+            { maker: "BMW2" }
+        ]
+    },
+    drive: function(){ return "dive"; }
 };
 
-var shoppinglist =  [
-    'apple',
-    'orange',
-    'pear'
+var array = [ 
+    "string",
+    100,
+    [ "embed", 200 ],
+    { car: "ford" },
+    function(){ return "dive"; }
 ];
